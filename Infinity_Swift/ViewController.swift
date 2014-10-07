37bfe69eb6b555b9ced5ef9cfd0becaee493a02c
sekaryo <asutamuse@gmail.com>
2014-10-08 00:31:07 +0900

    Initial Commit

---
+++ Infinity_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Infinity_Swift
+//
+//  Created by PxP_ss on 2014/10/08.
+//  Copyright (c) 2014年 pxp_ss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Infinity_SwiftTests/Infinity_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Infinity_SwiftTests.swift
+//  Infinity_SwiftTests
+//
+//  Created by PxP_ss on 2014/10/08.
+//  Copyright (c) 2014年 pxp_ss. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Infinity_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
