922df33e6f36bc4fd89e9d4d9338b43162e59a73
sekaryo <asutamuse@gmail.com>
2014-11-04 20:24:41 +0900

    iamgePickerを使ってカメラロールから画像と動画を選択する処理を追記

--- Infinity_Swift/AddUserViewController.swift
+++ Infinity_Swift/AddUserViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class AddUserViewController: UIViewController, UITextFieldDelegate {
+class AddUserViewController: UIViewController, UITextFieldDelegate
+                           , UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
     // Insert xib
     @IBOutlet weak var textFieldOfUserName  : UITextField!
@@ -22,13 +23,14 @@ class AddUserViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var pickerOfPicture      : UITextField!
  
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         self.pickerOfThemeColor.delegate = self
         self.pickerOfMovie.delegate      = self
         self.pickerOfFace.delegate       = self
         self.pickerOfPicture.delegate    = self
-    }   
+   }
     
     @IBAction func registUserInfo(sender: UIButton) {
     }
@@ -38,12 +40,59 @@ class AddUserViewController: UIViewController, UITextFieldDelegate {
         self.dismissViewControllerAnimated(true, completion: nil)
     }
     
+    func showPickerView(pickerId: NSInteger) {
+        // カラーピッカーの場合は呼び出さない
+        if (pickerId == 1){
+            return
+        }
+        
+        var picker = UIImagePickerController()
+        picker.delegate      = self
+        picker.sourceType    = UIImagePickerControllerSourceType.SavedPhotosAlbum
+        picker.allowsEditing = false
+        picker.view.tag      = pickerId
+        
+        // ムービーを呼び出す場合は、public.movieを指定
+        // ココらへんもう少しうまく場合分けしたい
+        if (pickerId == 2){
+            picker.mediaTypes = ["public.movie"];
+        }
+        
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
+        switch (picker.view.tag){
+          case 2:
+            let url = info[UIImagePickerControllerReferenceURL] as NSURL
+            self.pickerOfMovie.text = url.absoluteString
+            break;
+          case 3:
+            let url = info[UIImagePickerControllerReferenceURL] as NSURL
+            self.pickerOfFace.text = url.absoluteString
+            break;
+          case 4:
+            let url = info[UIImagePickerControllerReferenceURL] as NSURL
+            self.pickerOfPicture.text = url.absoluteString
+            break;
+          default:
+            println("指定されたpicker以外からのアクセスがあった")
+            break
+        }
+        
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        
+        self.showPickerView(textField.tag)
+        
         return false
     }
     
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 }
